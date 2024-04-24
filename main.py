28aeb50572952357af41eeb4293a62eb931e9e9f
RVM <rvmezin@inctrl.ru>
2024-04-24 15:55:31 +0300

    project file structure setup for later branching

---
+++ main.py
@@ -0,0 +1,21 @@
+from typing import Union
+from fastapi import FastAPI
+#from fastapi.templating import Jinja2Templates as J2T
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+
+env = Environment(
+    loader=FileSystemLoader('.'),
+    autoescape=select_autoescape(['html', 'xml'])
+)
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
