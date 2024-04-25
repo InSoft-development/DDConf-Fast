9c5491c3e21d9e1f9be64308214e852ed490726e
LinuxSin <mr-zo99@yandex.ru>
2024-04-25 22:26:29 +0300

    настроил вывод данных в шаблон dashboard

--- main.py
+++ main.py
@@ -15,18 +15,37 @@ app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="templates")
-PAC = [{"name":"Protocl_name"},
-       {"name":"Protocl_name"}]
+PAC = [{"name":"Номер протокола"}] # ПАК ОПТИ:
+LIC = [{"name":"Номер лицензии"}]  # Лицензия:
+PROT = [{"name": "МЭК 104"},
+        {"name": "OPC UA"},
+        {"name": "OPC UA"},
+        {"name": "МЭК 104"},
+        {"name": "МЭК 104"},
+        {"name": "МЭК 104"},
+        {"name": "МЭК 104"}] # Протоколы
 
-# NAME = [{"name": "Ivan", "type": "test"},
-#         {"name": "Roma", "type":"test"},
-#         {"name": "Alex", "type": "test"},
-#         {"name": "Bob",  "type": "test"},
-#         {"name": "Bob",  "type": "test"}]
+SETINTERF = [{"namber_row":"1.","ip":"10.44.55.56","adres":"00:1b:63:84:45:e6","text":"Up, configured"},
+             {"namber_row":"2.","ip":"-","adres":"00:1b:63:84:45:e7","text":"Down"},
+             {"namber_row":"3.","ip":"10.44.55.57","adres":"00:1b:63:84:45:e8","text":"Up, configured"},
+             {"namber_row":"4.","ip":"10.44.55.58","adres":"00:1b:63:84:45:e9","text":"Down"},
+             {"namber_row":"5.","ip":"10.44.55.58","adres":"00:1b:63:84:45:e9","text":"Down"},
+             {"namber_row":"6.","ip":"10.44.55.58","adres":"00:1b:63:84:45:e9","text":"Down"},
+             {"namber_row":"7.","ip":"10.44.55.58","adres":"00:1b:63:84:45:e9","text":"Down"},
+             {"namber_row":"8.","ip":"10.44.55.58","adres":"00:1b:63:84:45:e9","text":"Down"},
+             {"namber_row":"9.","ip":"10.44.55.58","adres":"00:1b:63:84:45:e9","text":"Down"},
+             {"namber_row":"10.","ip":"10.44.55.58","adres":"00:1b:63:84:45:e9","text":"Down"},
+             {"namber_row":"11.","ip":"10.44.55.58","adres":"00:1b:63:84:45:e9","text":"Down"},
+             {"namber_row":"12.","ip":"10.44.55.58","adres":"00:1b:63:84:45:e9","text":"Down"},
+             {"namber_row":"13.","ip":"10.44.55.58","adres":"00:1b:63:84:45:e9","text":"Down"}
+             
+             ]
+
+
 
 @app.get('/')
 async def name(request: Request):
-    return templates.TemplateResponse("dashboard.html",{"request": request, "name": "Ivan", "PAC_OPTI":PAC})
+    return templates.TemplateResponse("dashboard.html",{"request": request, "name": "Ivan", "PAC_OPTI":PAC, "LIC":LIC,"Protocols":PROT,"SETINTERF":SETINTERF})
 
 app.include_router(router_pages)
 # @app.get("/")
