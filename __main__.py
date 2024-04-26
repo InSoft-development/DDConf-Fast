1903d1efe646e49cbce559bdf8d8bb724bbc6653
rcz <romanecz00@gmail.com>
2024-04-26 20:51:05 +0300

    dd104: prototyped a data structure for the frontend

--- __main__.py
+++ __main__.py
@@ -16,6 +16,8 @@ app = FastAPI()
 
 # dashboard_data = {"active_protocols":{"DD104":"/pages/dd104", "OPC UA":"/pages/something", "SomeBullshit":"/pages/suckmydick"}, "license":"12321", "pac_num":"123123","network":[{"id":"1", "addr":"10.23.23.123", "macaddr":"eb:1a:b0:b1:cc", "status": "1"}, {"id":"2", "addr":"127.0.0.1", "macaddr":"eb:1a:b0:b1:c1", "status": "0"}]} # 0 = down, 1 = up
 
+# dd104_data = {"active_ld":{"id":"3", "processes":[{"id":"1", "main":"10.23.23.123:54678", "second":"10.23.23.123:54677", "status":"Running"}, {"id":"2", "main":"10.23.23.123:54679", "second":"-", "status":"Stopped"}, {"id":"3", "main":"sosat@kusat", "second":"-", "status":"Failed"}]}, "loadouts":[{"id":"1", "processes":[{"id":"1", "main":"10.23.23.202:54678", "second":"10.23.23.202:54677", "status":"Running"}, {"id":"2", "main":"10.23.23.202:54679", "second":"-", "status":"Stopped"}, {"id":"3", "main":"sosat@kusat", "second":"-", "status":"Failed"}]}, {"id":"2", "processes":[{"id":"1", "main":"10.23.23.13:54678", "second":"10.23.23.13:54677", "status":"Running"}, {"id":"2", "main":"10.23.23.13:54679", "second":"-", "status":"Stopped"}, {"id":"3", "main":"sosat@kusat", "second":"-", "status":"Failed"}]}, {"id":"3", "processes":[{"id":"1", "main":"10.23.23.123:54678", "second":"10.23.23.123:54677", "status":"Running"}, {"id":"3", "main":"10.23.23.123:54679", "second":"-", "status":"Stopped"}, {"id":"3", "main":"sosat@kusat", "second":"-", "status":"Failed"}]}]}
+
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
