1c326531352ed7980e20a1a4f66de395bfb23f2d
LinuxSin <mr-zo99@yandex.ru>
2024-04-27 17:59:37 +0300

    настроил переход со страницы dashboard.html на страницу Protokol_MEK_104.html, продолжается настройка шаблонов на странице Protokol_MEK_104

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from typing import Union
 from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from pages.router import router as router_pages
@@ -44,21 +45,66 @@ templates = Jinja2Templates(directory="templates")
 # RABPROF = [{"name":"Название профиляzz"}]
 
 
-# dashboard_data = {"active_protocols":[{"DD104":"/pages/dd104"}, {"OPC UA":"/pages/something"}, {"SomeBullshit":"/pages/suckmydick"}],
-#                   "License":"12321","pac_num":"12345",
-#                   "network":[{"id":"1", "addr":"10.23.23.123", "macaddr":"00:1b:63:84:45:e6", "status": "1"}, {"id":"2", "addr":"127.0.0.1", "DHCP":"no", "status": "0"}]}
+
 
 dashboard_data = {"active_protocols":{"DD104":"/pages/dd104", "OPC UA":"/pages/something", "SomeBullshit":"/pages/suckmydick"},
                   "license":"12321", "pac_num":"123123",
                   "network":[{"id":"1", "addr":"10.23.23.123", "macaddr":"eb:1a:b0:b1:cc", "status": "1"},
                              {"id":"2", "addr":"127.0.0.1", "macaddr":"eb:1a:b0:b1:c1", "status": "0"}]} # 0 = down, 1 = up
 
+DD104 = [{"num":"1","ipport":"10.30.44.15:23678678678","rezport":"norez","status":"запущен"},
+         {"num":"1","ipport":"10.30.44.15:23678678678","rezport":"norez","status":"on"},
+         {"num":"1","ipport":"10.30.44.15:23678678678","rezport":"norez","status":"on"},
+         {"num":"1","ipport":"10.30.44.15:23678678678","rezport":"norez","status":"on"},
+         {"num":"1","ipport":"10.30.44.15:23678678678","rezport":"norez","status":"on"},
+         {"num":"1","ipport":"10.30.44.15:23678678678","rezport":"norez","status":"on"},
+         {"num":"1","ipport":"10.30.44.15:23678678678","rezport":"norez","status":"on"},
+         {"num":"1","ipport":"10.30.44.15:23678678678","rezport":"norez","status":"on"},
+         {"num":"1","ipport":"10.30.44.15:23678678678","rezport":"norez","status":"on"},
+         {"num":"1","ipport":"10.30.44.15:23678678678","rezport":"norez","status":"on"},
+         {"num":"1","ipport":"10.30.44.15:23678678678","rezport":"norez","status":"on"},
+         {"num":"1","ipport":"10.30.44.15:23678678678","rezport":"norez","status":"on"},
+         {"num":"1","ipport":"10.30.44.15:23678678678","rezport":"norez","status":"on"}
+         ]
+
+dd104_data = {"active_ld":
+               {"id":"3", "processes":[{"id":"1", "main":"10.23.23.123:54678", "second":"10.23.23.123:54677", "status":"Running"},
+                                       {"id":"2", "main":"10.23.23.123:54679", "second":"-", "status":"Stopped"},
+                                       {"id":"3", "main":"sosat@kusat", "second":"-", "status":"Failed"}]},
+                           "loadouts":[{"id":"1", "processes":[{"id":"1", "main":"10.23.23.202:54678", "second":"10.23.23.202:54677", "status":"Running"},
+                                                               {"id":"2", "main":"10.23.23.202:54679", "second":"-", "status":"Stopped"},
+                                                               {"id":"3", "main":"sosat@kusat", "second":"-", "status":"Failed"}]},
+                                                               {"id":"2", "processes":[{"id":"1", "main":"10.23.23.13:54678", "second":"10.23.23.13:54677", "status":"Running"},
+                                                                                       {"id":"2", "main":"10.23.23.13:54679", "second":"-", "status":"Stopped"},
+                                                                                       {"id":"3", "main":"sosat@kusat", "second":"-", "status":"Failed"}]},
+                                                               {"id":"3", "processes":[{"id":"1", "main":"10.23.23.123:54678", "second":"10.23.23.123:54677", "status":"Running"},
+                                                                                       {"id":"3", "main":"10.23.23.123:54679", "second":"-", "status":"Stopped"},
+                                                                                       {"id":"3", "main":"sosat@kusat", "second":"-", "status":"Failed"}]}]}
+ 
+
 @app.get('/')
 async def name(request: Request):
     return templates.TemplateResponse("dashboard.html", {"request": request, "dashboard_data": dashboard_data})
 
-    
+@app.get('/dd104/')
+async def dd104_render(request: Request):
+    return templates.TemplateResponse("Protokol_MEK_104.html", {"request": request, "dashboard_data": dashboard_data, "DD104":DD104, })
 
+
+
+
+
+
+
+
+
+
+
+
+    
+# @app.get('/')
+# async def name(request: Request):
+#     return templates.TemplateResponse("Protokol_MEK_104.html", {"request": request, "rabprof":rabprof})
 
 
 app.include_router(router_pages)
@@ -70,3 +116,17 @@ app.include_router(router_pages)
 # @app.get("/items/{item_id}")
 # def read_item(item_id: int, q: Union[str, None] = None):
 #     return {"item_id": item_id, "q": q}
+# ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+# dd104_data = {"active_ld":
+#               {"id":"3", "processes":[{"id":"1", "main":"10.23.23.123:54678", "second":"10.23.23.123:54677", "status":"Running"},
+#                                       {"id":"2", "main":"10.23.23.123:54679", "second":"-", "status":"Stopped"},
+#                                       {"id":"3", "main":"sosat@kusat", "second":"-", "status":"Failed"}]},
+#                           "loadouts":[{"id":"1", "processes":[{"id":"1", "main":"10.23.23.202:54678", "second":"10.23.23.202:54677", "status":"Running"},
+#                                                               {"id":"2", "main":"10.23.23.202:54679", "second":"-", "status":"Stopped"},
+#                                                               {"id":"3", "main":"sosat@kusat", "second":"-", "status":"Failed"}]},
+#                                                               {"id":"2", "processes":[{"id":"1", "main":"10.23.23.13:54678", "second":"10.23.23.13:54677", "status":"Running"},
+#                                                                                       {"id":"2", "main":"10.23.23.13:54679", "second":"-", "status":"Stopped"},
+#                                                                                       {"id":"3", "main":"sosat@kusat", "second":"-", "status":"Failed"}]},
+#                                                               {"id":"3", "processes":[{"id":"1", "main":"10.23.23.123:54678", "second":"10.23.23.123:54677", "status":"Running"},
+#                                                                                       {"id":"3", "main":"10.23.23.123:54679", "second":"-", "status":"Stopped"},
+#                                                                                       {"id":"3", "main":"sosat@kusat", "second":"-", "status":"Failed"}]}]}
