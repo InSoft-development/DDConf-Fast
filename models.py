84eaf31f2dae9dc397044856bf617ce0faa0d734
rcz <romanecz00@gmail.com>
2024-05-31 20:31:38 +0300

    dd104: minor fixes

--- models.py
+++ models.py
@@ -1,5 +1,5 @@
- from pydantic import BaseModel
- from typing import Union, Annotated
+from pydantic import BaseModel
+from typing import Union, Annotated
  
 # class ProcessOperationTicket(BaseModel): #POT hehe
 # 	PID: int
