f87db7ef74db3e2723346a677fdc49b579df298f
rcz <romanecz00@gmail.com>
2024-06-10 19:33:03 +0300

    main: trying to solve the missing files problem wip7.7.2 - artem

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ BASE_DIR = Path(__file__).parent
 # 	BASE_DIR / "static",
 # ])
 
-app.mount("/dd104", StaticFiles(directory="static", html=True), name="static")
+# app.mount("/", StaticFiles(directory="static", html=True), name="static")
 
 origins = [
 	# "http://127.0.0.1:8080",
@@ -73,7 +73,7 @@ app.add_middleware(
 async def dd104_serve():
 	# templates = Jinja2Templates(directory="static/build")
 	# return templates.TemplateResponse("index.html", {"request": REQ})
-	return HTMLResponse(content='index.html', status_code=200)
+	return HTMLResponse(content=BASE_DIR/'index.html', status_code=200)
 
 
 @app.post("/dashboard")
