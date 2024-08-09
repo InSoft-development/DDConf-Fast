ba16a0709e7c0308daf5311de1be88c0c82bbfb5
I Sin <mr-zo99@yandex.ru>
2024-08-09 17:34:54 +0300

    typo

--- frontend/src/components/app-header/app-header.jsx
+++ frontend/src/components/app-header/app-header.jsx
@@ -20,7 +20,7 @@ const AppHeader = () => {
         const headerText = {
             '/': 'Дашбоард',
             '/dd104': 'Протокол МЭК 104',
-            '/profile-editor': 'Протокол МЭК 104'
+            '/profile-editor': 'Протокол МЭК 104',
 	    '/opcua': 'OPC UA'
         }
 
