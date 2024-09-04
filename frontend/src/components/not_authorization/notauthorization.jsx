75b08fe0c91c61f68ab996c20ed4bda60fcaf723
rcz <romanecz00@gmail.com>
2024-09-04 15:34:14 +0300

    dd104: fixed a bug that resulted in creation of 1 extra .service; removed frontend dir

--- frontend/src/components/not_authorization/notauthorization.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import style from './notauthorization.module.css';
-
-export const Notauthorization = () => {
-  return (
-    <div className={style.textNotAuto}>Без авторизации</div>
-  )
-}
-export default Notauthorization;
