4343ca8d7486dc8e7170a0a8b51861a7a26a1c7f
Nekrasov <nekrasov@inctrl.ru>
2024-09-13 18:35:18 +0300

    Страница OPC UA: - Добавлены actions, reducer и store (при монитировании страницы, происходит запрос за формами) - Динамическое добавление новых значений в форму стало работать корретно

--- frontend/src/pages/opc-ua/opc-ua.jsx
+++ frontend/src/pages/opc-ua/opc-ua.jsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styles from './opc-ua.module.css';
-import { Flex, Divider } from 'antd';
-import { DeleteOutlined, PlusOutlined } from '@ant-design/icons';
+import { useDispatch, useSelector } from 'react-redux';
+import { getOpcUaForm } from '../../services/actions/opc-ua';
+import { Flex, Divider, Spin } from 'antd';
+import { DeleteOutlined} from '@ant-design/icons';
 import { useForm, useFieldArray } from 'react-hook-form';
 import AuthSection from '../../components/form-elements/opc-ua/auth-section';
 import SubscriptionsSection from '../../components/form-elements/opc-ua/subscriptions-section';
@@ -9,132 +11,170 @@ import UrlSection from '../../components/form-elements/opc-ua/url-section';
 
 const OpcUa = () => {
 
-    const { register, handleSubmit, control, unregister, setValue } = useForm();
+    const dispatch = useDispatch();
+    const { form, getFormRequest } = useSelector(store => store.opcua);
+
+    const { register, handleSubmit, control, unregister, setValue, reset } = useForm({
+        shouldUnregister: true
+    });
     const { fields, append, remove } = useFieldArray({
         control,
         name: 'servers'
     })
 
+    useEffect(() => {
+        dispatch(getOpcUaForm())
+    }, [])
+
+    useEffect(() => {
+        console.log(form);
+
+        reset(form)
+    }, [form])
+
     const onSubmit = (data) => {
         console.log(data);
+        console.log(JSON.stringify(data));
     }
 
     return (
-        <form onSubmit={handleSubmit(onSubmit)}
-            className={`text text-18 ${styles.form}`}>
-            {/* restore */}
-            <div className={styles.restore}>
-                <label htmlFor="restore" className='fw-b mr-10 ml-10'>Восстановить соединение после падения</label>
-                <input
-                    id='restore'
-                    {...register('restore')}
-                    type="checkbox"
-                />
-            </div>
-            {/* servers fields */}
-            <div className='mt-20'>
-                {
-                    fields.map((server, index) => (
-                        <div key={server.id} className={styles.server}>
-                            {/* title & delete button */}
-                            <h3 className={styles.serverTitle}>Сервер {index + 1}</h3>
-                            <button type="button"
-                                className={`button btn-red btn-circle ${styles.serverDeleteBtn}`}
-                                onClick={e => remove(index)}
-                            >
-                                <DeleteOutlined />
-                            </button>
-                            {/* URL's */}
-                            <div className={styles.serverInputsRows}>
-                                <UrlSection
-                                    index={index}
-                                    register={register}
-                                    control={control}
-                                />
-                                <Divider />
-                                {/* Authorization Type */}
-                                <Flex align='center' justify='space-between'>
-                                    <label
-                                        htmlFor={`servers.${index}.utoken_type`}
-                                        className='fw-b'
-                                    >Тип авторизации:</label>
-                                    <select
-                                        defaultValue={'anonymous'}
-                                        id={`servers.${index}.utoken_type`}
-                                        {...register(`servers.${index}.utoken_type`)}
-                                        className='opc-input'
+        <>
+            {getFormRequest ? (
+                <Flex
+                    align='center' 
+                    className='text text_type_main mt-10'
+                >
+                    <span className=' ml-10 mr-10'>Идёт загрузка формы</span>
+                    <Spin></Spin>
+                </Flex>
+            ) : (
+                <form onSubmit={handleSubmit(onSubmit)}
+                    className={`text text-18 ${styles.form}`}>
+                    {/* restore */}
+                    <div className={styles.restore}>
+                        <label htmlFor="restore" className='fw-b mr-10 ml-10'>Восстановить соединение после падения</label>
+                        <input
+                            id='restore'
+                            {...register('restore')}
+                            type="checkbox"
+                        />
+                    </div>
+                    {/* servers fields */}
+                    <div className='mt-20'>
+                        {
+                            fields.map((server, index) => (
+                                <div key={server.id} className={styles.server}>
+                                    {/* title & delete button */}
+                                    <h3 className={styles.serverTitle}>Сервер {index + 1}</h3>
+                                    <button type="button"
+                                        className={`button btn-red btn-circle ${styles.serverDeleteBtn}`}
+                                        onClick={e => remove(index)}
                                     >
-                                        <option value="anonymous">Без авторизации</option>
-                                        <option value="certificate">По сертификату</option>
-                                        <option value="username">По логину и паролю</option>
-                                    </select>
-                                </Flex>
-                                {/*  */}
-                                <AuthSection
-                                    register={register}
-                                    index={index}
-                                    control={control}
-                                    unregister={unregister}
-                                    setValue={setValue}
-                                />
-                                <Divider />
-                                {/* Security Policy */}
-                                <Flex align='center' justify='space-between'>
-                                    <label
-                                        htmlFor={`servers.${index}.secpolicy`}
-                                        className='fw-b'
-                                    >Политика безопасности:</label>
-                                    <select
-                                        defaultValue={'None'}
-                                        id={`servers.${index}.secpolicy`}
-                                        {...register(`servers.${index}.secpolicy`)}
-                                        className='opc-input'
-                                    >
-                                        <option value="None">None</option>
-                                        <option value="Basic256Sha256 - Sign">Basic256Sha256 - Sign</option>
-                                        <option value="Basic256Sha256 - Sign & Encrypt">Basic256Sha256 - Sign & Encrypt</option>
-                                        <option value="Aes128_Sha256_RsaOaep - Sign">Aes128_Sha256_RsaOaep - Sign</option>
-                                        <option value="Aes128_Sha256_RsaOaep - Sign & Encrypt">Aes128_Sha256_RsaOaep - Sign & Encrypt</option>
-                                        <option value="Aes256_Sha256_RsaPss - Sign">Aes256_Sha256_RsaPss - Sign</option>
-                                        <option value="Aes256_Sha256_RsaPss - Sign & Encrypt">Aes256_Sha256_RsaPss - Sign & Encrypt</option>
-                                    </select>
-                                </Flex>
-                                <Flex align='center' justify='space-between'>
-                                    <label
-                                        htmlFor={`servers.${index}.mesmode`}
-                                        className='fw-b'
-                                    >Режим шифрования <br /> сообщений:</label>
-                                    <select
-                                        defaultValue={'None'}
-                                        id={`servers.${index}.mesmode`}
-                                        {...register(`servers.${index}.mesmode`)}
-                                        className='opc-input'
-                                    >
-                                        <option value="None">None</option>
-                                        <option value="Sign">Sign</option>
-                                        <option value="Sign & Encrypt">Sign & Encrypt</option>
-                                    </select>
-                                </Flex>
-                                <Divider />
-                                {/* Subscriptions */}
-                                <SubscriptionsSection
-                                    register={register}
-                                    index={index}
-                                    control={control}
-                                    unregister={unregister}
-                                    setValue={setValue}
-                                />
-                            </div>
-                        </div>
-                    ))
-                }
-            </div>
+                                        <DeleteOutlined />
+                                    </button>
+                                    {/* URL's */}
+                                    <div className={styles.serverInputsRows}>
+                                        <UrlSection
+                                            index={index}
+                                            register={register}
+                                            control={control}
+                                        />
+                                        <Divider />
+                                        {/* Authorization Type */}
+                                        <Flex align='center' justify='space-between'>
+                                            <label
+                                                htmlFor={`servers.${index}.utoken_type`}
+                                                className='fw-b label-required-symbol'
+                                            >Тип авторизации:</label>
+                                            <select
+                                                defaultValue={'anonymous'}
+                                                id={`servers.${index}.utoken_type`}
+                                                {...register(`servers.${index}.utoken_type`)}
+                                                className='opc-input'
+                                            >
+                                                <option value="anonymous">Без авторизации</option>
+                                                {/* <option value="certificate">По сертификату</option> */}
+                                                <option value="username">По логину и паролю</option>
+                                            </select>
+                                        </Flex>
+                                        {/*  */}
+                                        <AuthSection
+                                            register={register}
+                                            index={index}
+                                            control={control}
+                                            unregister={unregister}
+                                            setValue={setValue}
+                                        />
+                                        <Divider />
+                                        {/* Security Policy */}
+                                        <Flex align='center' justify='space-between'>
+                                            <label
+                                                htmlFor={`servers.${index}.secpolicy`}
+                                                className='fw-b label-required-symbol'
+                                            >Политика безопасности:</label>
+                                            <select
+                                                defaultValue={'None'}
+                                                id={`servers.${index}.secpolicy`}
+                                                {...register(`servers.${index}.secpolicy`)}
+                                                className='opc-input'
+                                            >
+                                                <option value="None">None</option>
+                                                <option value="Basic256Sha256 - Sign">Basic256Sha256 - Sign</option>
+                                                <option value="Basic256Sha256 - Sign & Encrypt">Basic256Sha256 - Sign & Encrypt</option>
+                                                <option value="Aes128_Sha256_RsaOaep - Sign">Aes128_Sha256_RsaOaep - Sign</option>
+                                                <option value="Aes128_Sha256_RsaOaep - Sign & Encrypt">Aes128_Sha256_RsaOaep - Sign & Encrypt</option>
+                                                <option value="Aes256_Sha256_RsaPss - Sign">Aes256_Sha256_RsaPss - Sign</option>
+                                                <option value="Aes256_Sha256_RsaPss - Sign & Encrypt">Aes256_Sha256_RsaPss - Sign & Encrypt</option>
+                                            </select>
+                                        </Flex>
+                                        <Flex align='center' justify='space-between'>
+                                            <label
+                                                htmlFor={`servers.${index}.mesmode`}
+                                                className='fw-b'
+                                            >Режим шифрования <br /> сообщений:</label>
+                                            <select
+                                                defaultValue={'None'}
+                                                id={`servers.${index}.mesmode`}
+                                                {...register(`servers.${index}.mesmode`)}
+                                                className='opc-input'
+                                            >
+                                                <option value="None">None</option>
+                                                <option value="Sign">Sign</option>
+                                                <option value="Sign & Encrypt">Sign & Encrypt</option>
+                                            </select>
+                                        </Flex>
+                                        <Divider />
+                                        {/* Subscriptions */}
+                                        <SubscriptionsSection
+                                            register={register}
+                                            index={index}
+                                            control={control}
+                                            unregister={unregister}
+                                            setValue={setValue}
+                                        />
+                                    </div>
+                                </div>
+                            ))
+                        }
+                    </div>
 
-            <div className='mt-10'>
-                <button type="button" className="button btn-green mr-4" onClick={e => append()}>Добавить сервер</button>
-                <button type="submit" className='button btn-green'>Отправить</button>
-            </div>
-        </form>
+                    <div className='mt-10'>
+                        <button type="button"
+                            className="button btn-green mr-4"
+                            onClick={e => append({
+                                url1: '',
+                                url2_exists: false,
+                                utoken_type: "anonymous",
+                                secpolicy: "None",
+                                mesmode: "None",
+                                subscriptions: []
+                            })}
+                        >Добавить сервер</button>
+                        <button type="submit" className='button btn-green'>Отправить</button>
+                    </div>
+                </form>
+            )}
+        </>
     )
 
 }
