f29de761c0a3f2fad0b31afd8eb34c6e5ad3e58b
Nekrasov <nekrasov@inctrl.ru>
2024-09-19 18:34:24 +0300

    Редактор профилей МЭК 104 - Страница profile-editor полность переделана (компонент Table был связан с нативной формой, полностью поддерживает CRUD операции)

--- frontend/src/components/app/app.jsx
+++ frontend/src/components/app/app.jsx
@@ -7,8 +7,8 @@ import Layout from '../layout/layout';
 import SignIn from '../../pages/sign-in/sign-in';
 import Dd104 from '../../pages/dd104/dd104';
 import Dashboard from '../../pages/dashboard/dashboard';
-import ProfileEditor from '../../pages/profile-editor/profile-editor';
 import OpcUa from '../../pages/opc-ua/opc-ua';
+import ProfileEditor from '../../pages/profile-editor/profile-editor';
 // services
 import {getDeviceFeatures} from '../../services/actions/dashboard'
 import { getProfiles } from '../../services/actions/profile';
@@ -29,15 +29,13 @@ const App = () => {
                 <Route path='/' element={<Layout/>}>
                     <Route index element={<Dashboard/>}></Route>
                     <Route path='dd104' element={<Dd104/>}></Route>
-                    <Route path='profile-editor' element={<ProfileEditor/>}></Route>
                     <Route path='opcua' element={<OpcUa/>}></Route>
+                    <Route path='profile-editor' element={<ProfileEditor/>}></Route>
                 </Route>
-                <Route path='/sign-in' element={<SignIn/>}></Route>
+                <Route path='/login' element={<SignIn/>}></Route>
             </Routes>
         </>
     );
 }
 
 export default App;
-
-
