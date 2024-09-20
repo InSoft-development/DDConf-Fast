64768a6c02473931106f7607632a5ef12a6bb991
Nekrasov <nekrasov@inctrl.ru>
2024-09-20 19:04:53 +0300

    Страница Login - добавлен react-hook-form

--- frontend/src/components/app/app.jsx
+++ frontend/src/components/app/app.jsx
@@ -1,19 +1,15 @@
-import { useDispatch } from 'react-redux';
 import { Routes, Route } from 'react-router-dom';
-
 
 // pages
 import Layout from '../layout/layout';
 
-import SignIn from '../../pages/sign-in/sign-in';
+import Login from '../../pages/sign-in/sign-in';
 import Dd104 from '../../pages/dd104/dd104';
 import Dashboard from '../../pages/dashboard/dashboard';
 import OpcUa from '../../pages/opc-ua/opc-ua';
 import ProfileEditor from '../../pages/profile-editor/profile-editor';
 
 const App = () => {
-
-
 
     return (
         <>
@@ -24,7 +20,7 @@ const App = () => {
                     <Route path='opcua' element={<OpcUa/>}></Route>
                     <Route path='profile-editor' element={<ProfileEditor/>}></Route>
                 </Route>
-                <Route path='/login' element={<SignIn/>}></Route>
+                <Route path='/login' element={<Login/>}></Route>
             </Routes>
         </>
     );
