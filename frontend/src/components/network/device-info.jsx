c5f11556716c2bc03ce89eca4243f3b3461d05fe
Nekrasov <nekrasov@inctrl.ru>
2024-10-14 15:55:11 +0300

    Страница Network, нормализация uptime

--- frontend/src/components/network/device-info.jsx
+++ frontend/src/components/network/device-info.jsx
@@ -1,19 +1,34 @@
 import React from 'react';
-import { Flex } from 'antd';
-
 import styles from './device-info.module.scss';
 
 const DeviceInfo = ({ device }) => {
 
+    const normalizeDate = (timestamp) => {
+        const date = new Date(timestamp);
+
+        const year = date.getFullYear();
+        const month = date.getMonth() + 1;
+        const day = date.getDate();
+
+        const hours = date.getHours();
+        const minutes = date.getMinutes();
+        const seconds = date.getSeconds();
+
+        return `${year}/${month}/${day} ${hours}:${minutes}:${seconds}`;
+    }
+   
     return (
         <div className={styles.device}>
             <div className={styles.deviceRow}>
                 <div className='text_type_main_default text_bold'>Device:</div>
-                <div className='text_type_main_default'>{device?.device || ''}</div>
+                <div className='text_type_main_default'>
+                    {device?.device || ''}</div>
             </div>
             <div className={styles.deviceRow}>
                 <div className='text_type_main_default text_bold'>Uptime:</div>
-                <div className='text_type_main_default'>{device?.uptime || ''}</div>
+                <div className='text_type_main_default'>
+                    {device === null ? '' : normalizeDate(device.uptime)}
+                </div>
             </div>
             <div className={styles.deviceRow}>
                 <div className='text_type_main_default text_bold'>MAC:</div>
