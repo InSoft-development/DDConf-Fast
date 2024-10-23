c553c13f0b327df60265cdff4c53e8ebb8be3fb6
Nekrasov <nekrasov@inctrl.ru>
2024-10-23 15:04:58 +0300

    Миграция на RTK: - Создан слайс состояния для страницы Dashboard

---
+++ frontend/src/services/slices/dashboard.js
@@ -0,0 +1,111 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { request } from '../api';
+import { checkResponce } from '../../utils/checkResponce';
+
+const initialState = {
+    serial: '',
+    license: '',
+    protocols: [],
+    network: [],
+
+    fetchInitialStatus: false,
+    fetchInitialError: false,
+    fetchProtocolsStatus: false,
+    fetchProtocolsError: false,
+    fetchNetworkStatus: false,
+    fetchNetworkError: false,
+};
+
+const fetchInitial = createAsyncThunk(
+    'dashbord/fetchInitial',
+    async (_, { rejectWithValue }) => {
+        try{
+            const responce = await request('dashboard', 'fetch_initial');
+            const data = await checkResponce(responce);
+    
+            return data;
+        }catch(error){
+            rejectWithValue(error);
+        }
+    }
+);
+
+const fetchProtocols = createAsyncThunk(
+    'dashbord/fetchProtocols',
+    async (_, { rejectWithValue }) => {
+        try{
+            const responce = await request('dashboard', 'fetch_protocols');
+            const data = await checkResponce(responce);
+    
+            return data;
+        }catch(error){
+            rejectWithValue(error);
+        }
+    }
+);
+
+const fetchNetwork = createAsyncThunk(
+    'dashboard/fetchNetwork',
+    async (_, { rejectWithValue }) => {
+        try {
+            const responce = await request('dashboard', 'fetch_net');
+            const data = await checkResponce(responce);
+
+            return data;
+        } catch (error) {
+            rejectWithValue(error)
+        }
+    }
+);
+
+const dashboardSlice = createSlice({
+    name: 'dashboard',
+    initialState: initialState,
+    extraReducers: (builder) => {
+        builder.addCase(fetchInitial.pending, state => {
+            state.fetchInitialStatus = 'pending';
+            state.fetchInitialError = null;
+        });
+        builder.addCase(fetchInitial.fulfilled, (state, action) => {
+            state.fetchInitialStatus = 'fulfilled';
+            state.fetchInitialError = null;
+            state.serial = action.payload.result.serial;
+            state.license = action.payload.result.license;
+        });
+        builder.addCase(fetchInitial.rejected, (state, action) => {
+            state.fetchInitialStatus = 'rejected';
+            state.fetchInitialError = action.payload;
+        });
+
+        builder.addCase(fetchProtocols.pending, state => {
+            state.fetchProtocolsStatus = 'pending';
+            state.fetchProtocolsError = null;
+        });
+        builder.addCase(fetchProtocols.fulfilled, (state, action) => {
+            state.fetchProtocolsStatus = 'fulfilled';
+            state.fetchProtocolsError = null;
+            state.protocols = action.payload.result.protocols;
+        });
+        builder.addCase(fetchProtocols.rejected, (state, action) => {
+            state.fetchProtocolsStatus = 'rejected';
+            state.fetchProtocolsError = action.payload;
+        });
+
+        builder.addCase(fetchNetwork.pending, state => {
+            state.fetchNetworkStatus = 'pending';
+            state.fetchNetworkError = null;
+        });
+        builder.addCase(fetchNetwork.fulfilled, (state, action) => {
+            state.fetchNetworkStatus = 'fulfielled';
+            state.fetchNetworkError = null;
+            state.protocols = action.payload.result;
+        });
+        builder.addCase(fetchNetwork.rejected, (state, action) => {
+            state.fetchNetworkStatus = 'rejected';
+            state.fetchNetworkError = action.payload;
+        });
+    }
+});
+
+export { fetchInitial, fetchProtocols, fetchNetwork };
+export default dashboardSlice.reducer;
