81688976b6c852cfd9e204771969850eb952faff
Nekrasov <nekrasov@inctrl.ru>
2024-11-07 15:43:06 +0300

    Код-ревью: - Вынесение размером модулей страниц в scss переменные

--- frontend/src/components/app-header/app-header.jsx
+++ frontend/src/components/app-header/app-header.jsx
@@ -4,7 +4,7 @@ import { MenuOutlined } from '@ant-design/icons';
 import { OPEN_SIDEBAR } from '../../services/actions/modals';
 import styles from './app-header.module.scss';
 
-const AppHeader = ({title}) => {
+const AppHeader = ({ title }) => {
 
     const dispatch = useDispatch();
 
@@ -14,16 +14,18 @@ const AppHeader = ({title}) => {
 
     return (
         <header className={styles.header}>
-            <div className={styles.upLine}></div>
-            <div className={styles.downLine}></div>
-            <h6 className='wrapper text_type_main_small text_bold'>ДД Конфигуратор</h6>
-            <h4 className='wrapper text_type_main_extra-large'>
-                <MenuOutlined className='mr-14' 
-                    onClick={e => onMenuClickHandler()}
-                    style={{fontSize: 34}}
-                />
-                {title}
-            </h4>
+            <div className={styles.upLine}>
+                <h6 className='wrapper text_type_main_small text_bold'>ДД Конфигуратор</h6>
+            </div>
+            <div className={styles.middleLine}>
+                <h4 className='wrapper text_type_main_extra-large'>
+                    <MenuOutlined className='mr-14'
+                        onClick={e => onMenuClickHandler()}
+                        style={{ fontSize: 34 }}
+                    />
+                    {title}
+                </h4>
+            </div>
         </header>
     );
 }
